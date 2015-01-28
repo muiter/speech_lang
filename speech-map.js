170026e972c9b8bbc0bcfe55a4fb809747eb7358
Matthew Muiter <muiter@uw.edu>
2015-01-28 00:57:04 -0800

    current files for speech_language project

---
+++ speech-map.js
@@ -0,0 +1,132 @@
+$(function(){
+
+var map;
+var speech_business = new google.maps.LatLng(47.634826, -122.137701);
+
+function initialize() {
+
+var roadAtlasStyles = [
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 60
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 30
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ef8c25"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#b6c54c"
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "saturation": -40
+            }
+        ]
+    },
+    {}
+]
+
+  var mapOptions = {
+    zoom: 12,
+    center: speech_business,
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
+    }
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+var marker = new google.maps.Marker({
+      position: speech_business,
+      map: map
+  });
+
+  var styledMapOptions = {
+    name: 'US Road Atlas'
+  };
+
+  var usRoadMapType = new google.maps.StyledMapType(
+      roadAtlasStyles, styledMapOptions);
+
+  map.mapTypes.set('usroadatlas', usRoadMapType);
+  map.setMapTypeId('usroadatlas');
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+});
